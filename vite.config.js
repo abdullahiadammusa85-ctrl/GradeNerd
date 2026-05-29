264b26ab16c95030799aa39f77c83ad8863a166e
abdullahiadammusa85-ctrl <abdullahiadammusa85@gmail.com>
2026-05-29 21:25:47 +0100

    fix permission error

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({plugins: [react()],})
+"scripts": {
+    "dev": "vite",
+    "build": "npx vite build",
+    "preview": "npx vite preview"
+  }
